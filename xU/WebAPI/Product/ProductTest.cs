6bb420539c1bcd7764f2e3154e4c66dd66ab835c
DESKTOP-GB680PK\mak27 <mak27arr@gmail.com>
2022-07-11 22:54:02 +0300

    Add one test

--- xU/WebAPI/Product/ProductTest.cs
+++ xU/WebAPI/Product/ProductTest.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Database.Entities;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace xU.WebAPITest.Product
+namespace xU.WebAPITest.ProductTests
 {
-    public class ProductTestsControllerTests : IClassFixture<WebApplicationFactory<WebAPI.Program>>
+    public class ProductTestsControllerTests : IClassFixture<WebApplicationFactory<WebAPI.Program>>, IDisposable
     {
         private readonly string _controlerName = "Products";
         readonly HttpClient _client;
@@ -20,7 +25,28 @@ namespace xU.WebAPITest.Product
         {
             var response = await _client.GetAsync($"/api/{_controlerName}");
             response.EnsureSuccessStatusCode();
-            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+            var expected = "application/json; charset=utf-8";
+            var actual = response.Content.Headers.ContentType.ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task AddProduct()
+        {
+            var product = new Product()
+            {
+                Name = "Test name",
+                Barcode = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.FFFZ")                   
+            };
+            var response = await _client.PostAsync($"/api/{_controlerName}", new StringContent(JsonConvert.SerializeObject(product), Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+            var id = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
         }
     }
 }
