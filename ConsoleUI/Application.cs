2e749da5aa2ac38bf67890cd82b3fc4ca017d5cf
DESKTOP-GB680PK\mak27 <mak27arr@gmail.com>
2022-08-04 22:45:51 +0300

    Add Autofac DI

---
+++ ConsoleUI/Application.cs
@@ -0,0 +1,19 @@
+﻿using DemoLibrary;
+
+namespace ConsoleUI
+{
+    public class Application : IApplication
+    {
+        IBusinessLogic _businessLogic;
+
+        public Application(IBusinessLogic businessLogic)
+        {
+            _businessLogic = businessLogic;
+        }
+
+        public void Run()
+        {
+            _businessLogic.ProcessData();
+        }
+    }
+}
