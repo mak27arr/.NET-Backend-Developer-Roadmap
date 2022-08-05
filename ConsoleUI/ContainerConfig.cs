2976bbe83e0324bf24c3d87c071583533d8ba394
DESKTOP-GB680PK\mak27 <mak27arr@gmail.com>
2022-08-05 21:57:12 +0300

    Autofac

---
+++ ConsoleUI/ContainerConfig.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using DemoLibrary;
+using System.Reflection;
+
+namespace ConsoleUI
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Application>().As<IApplication>();
+            builder.RegisterType<BetterBusinessLogic>().As<IBusinessLogic>();
+
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
+                .Where(t => t.Namespace.Contains("Utilities"))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+
+            return builder.Build();
+        }
+    }
+}
