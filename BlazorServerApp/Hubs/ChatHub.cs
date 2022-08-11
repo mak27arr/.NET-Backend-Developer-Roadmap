b11826c152572075fd36328ba76df715a938589a
DESKTOP-GB680PK\mak27 <mak27arr@gmail.com>
2022-08-11 22:44:47 +0300

    SignalR start

---
+++ BlazorServerApp/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using BlazorServerApp.Enum;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace BlazorServerApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            return Clients.All.SendAsync(nameof(Message.ReceiveMessage), user, message);
+        }
+    }
+}
